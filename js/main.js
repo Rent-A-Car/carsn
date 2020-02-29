29f179585d527c3514fb4afa3faf65cd343a8457
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-02-29 23:27:58 +0100

    Sat Feb 29 23:27:57 CET 2020

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+//console.log(jsonCarData);
 
+$(document).ready(function(){
+//carsalbum
+var obj = JSON.parse(jsonCarData);
+var b = obj.length;
+console.log(obj);
+var CONTAINER = $("#carsalbum");
+for (var i=0; i<b; i++){
+
+    CONTAINER.append('<div id="ClCa-'+i.toString()+'" class="col-md-4"></div>');
+
+
+
+
+
+}
+
+
+});
