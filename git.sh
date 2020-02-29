c820300a9110e731a9e3ae9f77393e5548a0ffa1
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-02-29 14:47:55 +0100

    Sat Feb 29 14:47:55 CET 2020

--- git.sh
+++ git.sh
@@ -1,1 +1,1 @@
-git add . && git commit -m "` date `" && git push origin master
+echo "var jsonCarData = '`cat data.json`'; " > js/data.js && git add . && git commit -m "` date `" && git push origin master
