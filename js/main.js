432c4cc142a0df0487721778532cd3832c2f536c
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-03-02 19:22:58 +0100

    Mon Mar  2 19:22:58 CET 2020

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //console.log(jsonCarData);
-
+$(".nav-link").on('click', function(event){
+     $("#navbarNav").collapse("hide");
+    //(... rest of your JS code)
+});
 $(document).ready(function(){
 //carsalbum
 jQuery.get("https://shareimg.gq/api/rw", function(data, status){
@@ -7,7 +10,7 @@ jQuery.get("https://shareimg.gq/api/rw", function(data, status){
      for (var i=0; i<d.length;i++){
      if (i==0){
      $(".spanFromOut").html(d[0]);
-     const regexp = /^\d+/gm;
+     const regexp = /^(\d,\d|\d)/gm;
      
      let fromto = d[0].match(regexp);
      
@@ -17,6 +20,7 @@ jQuery.get("https://shareimg.gq/api/rw", function(data, status){
      }
      if (i==1){
      $(".spanWhoThink").html(d[1]);
+     $("#COM").html("");
      }
      if(i>1){
      $("#COM").append('<div class="fb-post rounded mt-3 mt-md-0  mb-3" data-href="https://www.facebook.com'+d[i]+'"  data-show-text="false"></div>');
