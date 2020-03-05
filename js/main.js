4805764f71972ab77b59c3598a9a94c6292890a8
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-03-05 08:40:15 +0100

    Thu Mar  5 08:40:15 CET 2020

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 //console.log(jsonCarData);
-
-const DEBUGNOSCRIPT = 1;
+//pre load document
+const REVIEW = 0;
 $(".nav-link").on('click', function(event){$("#navbarNav").collapse("hide");});
+
+//on document load
 document.addEventListener('DOMContentLoaded', function(){
 //carsalbum
 var obj = JSON.parse(jsonCarData);
@@ -17,15 +19,16 @@ var opis = obj[i]["details"]["text"];
 $.each(cword, function( key, value ) {
    opis=opis.split("{"+key+"}").join(value);
 });
-
-
-    CONTAINER.append('<div id="ClCa-'+i.toString()+'" class="col-md-4"><div class="card mb-4 box-shadow bg-light text-center border border-dark" ><div class="card-header bg-light">'+obj[i]["details"]["name"]+'</div><img class="card-img-bottom" src="'+obj[i]["img"][1]+"0."+obj[i]["img"][2]+'" alt="'+obj[i]["details"]["name"]+'"><div class="card-body"><h5 class="card-title">'+obj[i]["details"]["fname"]+'</h5><p class="card-text">'+opis+'</p><a href="#" class="btn btn-primary">Подробниее</a></div></div></div>');
+CONTAINER.append('<div id="ClCa-'+i.toString()+'" class="col-md-4"><div class="card mb-4 box-shadow bg-light text-center border border-dark" ><div class="card-header bg-light">'+obj[i]["details"]["name"]+'</div><img class="card-img-bottom lazyimg" lazysrc="'+obj[i]["img"][1]+"0."+obj[i]["img"][2]+'" alt="'+obj[i]["details"]["name"]+'"><div class="card-body"><h5 class="card-title">'+obj[i]["details"]["fname"]+'</h5><p class="card-text">'+opis+'</p><a href="#" class="btn btn-primary">Подробниее</a></div></div></div>');
 
 }
 
 //lazy
 $(".homeIMG").addClass("lazy-off");
-
+$( ".lazyimg" ).each(function() {
+  $(this).attr("src",$(this).attr("lazysrc"));
+  $(this).removeClass("lazyimg");
+});
 
 
 
@@ -33,7 +36,7 @@ $(".homeIMG").addClass("lazy-off");
 
 
 //reviews
-if (!DEBUGNOSCRIPT){
+if (REVIEW){
 jQuery.get("https://shareimg.gq/api/rw", function(data){
      var d=JSON.parse(data);
      for (var i=0; i<d.length;i++){
