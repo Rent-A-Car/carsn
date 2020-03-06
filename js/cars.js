ee7c90819c26ac1764931585eafaef42fa108262
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-03-06 20:38:16 +0100

    Fri Mar  6 20:38:16 CET 2020

--- js/cars.js
+++ js/cars.js
@@ -72,7 +72,7 @@ myWindow.document.write(`
     <!--    CSS      -->
 
     <link rel="stylesheet" href="css/all.css?vbnvcgnlj">
-    <link rel="stylesheet" href="css/carsP.css?vbnnvcgnlj">
+    <!-- <link rel="stylesheet" href="css/carsP.css?vbnnvcgnlj"> -->
 </head>
 
 <body id="home" style="background-color:#e9ecef;">
@@ -149,7 +149,7 @@ myWindow.document.write(`
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <script src="js/all.js?hdbhdb"></script>
-        <script src="js/carsP.js?hdbhdb"></script>
+        <!-- <script src="js/carsP.js?hdbhdb"></script> -->
 
 </body>
 
