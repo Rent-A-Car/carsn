c204b373fffef2b0d14524f1ef284eaad968dd3f
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-03-14 21:51:18 +0100

    Sat Mar 14 21:51:18 CET 2020

--- js/cars.js
+++ js/cars.js
@@ -77,166 +77,144 @@ var myWindow = window.open("","_self","");//i.toString()+"-"+obj[i], "");
 myWindow.document.write(`
 <!doctype html>
 <html>
-
-<head>
-    <!-- Required meta tags -->
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Аренда автомобилей (`+obj[i]["details"]["name"]+`)</title>
-    <!--    CSS      -->
-
-    <link rel="stylesheet" href="css/all.css?vbnvcgnlj">
-    <style type="text/css">
-    .social a {
-    text-align: center;
-    width: 48px;
-    height: 48px;
-    float: left;
-    background: #fff;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 4px rgba(0,0,0,0.15), inset 0 0 50px rgba(0,0,0,0.1);
-    border-radius: 24px;
-    margin: 0 10px 10px 0;
-    padding: 6px;
-    color: #000;
-    }
-    .instagram a:hover {background: #3f729b; color: #fff;}
-    .facebook a:hover {background: #3b5998; color: #fff;}
-    .whatsapp a:hover {background: #50b154; color: #fff;}
-    .viber a:hover {background: #665CAC; color: #fff;}
-    
-    
-    </style>
-    <!-- <link rel="stylesheet" href="css/carsP.css?vbnnvcgnlj"> -->
-</head>
-
-<body id="home" style="background-color:#e9ecef;">
-
-    <nav class="navbar navbar-dark bg-dark navbar-expand-md sticky-top">
-        <h1 class=" navbar-brand">Аренда авто Черногория</h1>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse d-md-flex justify-content-end" id="navbarNav">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Аренда автомобилей (`+obj[i]["details"]["name"]+`)</title>
+      <!--    CSS      -->
+      <link rel="stylesheet" href="css/all.css?vbnvcgnlj">
+      <style>
+         .social a {
+         text-align: center;
+         width: 48px;
+         height: 48px;
+         float: left;
+         background: #fff;
+         border: 1px solid #ccc;
+         box-shadow: 0 2px 4px rgba(0,0,0,0.15), inset 0 0 50px rgba(0,0,0,0.1);
+         border-radius: 24px;
+         margin: 0 10px 10px 0;
+         padding: 6px;
+         color: #000;
+         }
+         .instagram a:hover {background: #3f729b; color: #fff;}
+         .facebook a:hover {background: #3b5998; color: #fff;}
+         .whatsapp a:hover {background: #50b154; color: #fff;}
+         .viber a:hover {background: #665CAC; color: #fff;}
+      </style>
+      <!-- <link rel="stylesheet" href="css/carsP.css?vbnnvcgnlj"> -->
+   </head>
+   <body id="home">
+      <nav class="navbar navbar-dark bg-dark navbar-expand-md sticky-top">
+         <h1 class=" navbar-brand">Аренда авто Черногория</h1>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false">
+         <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse d-md-flex justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.html">Главная</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="cars.html">Машины (`+obj[i]["details"]["name"]+`)</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contacts.html">Контакты</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="reviews.html">Отзывы</a>
-                </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="index.html">Главная</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link active" href="cars.html">Машины (`+obj[i]["details"]["name"]+`)</a>
+               </li>
+               <!--
+                  <li class="nav-item" >
+                      <a class="nav-link" href="#servises" >Дополнительные услуги</a>
+                  </li>
+                  -->
+               <li class="nav-item">
+                  <a class="nav-link" href="contacts.html">Контакты</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="reviews.html">Отзывы</a>
+               </li>
             </ul>
-        </div>
-    </nav>
-
-
-
-<div class="container-fluid jumbotron" >
-  
-        <div class="row">
-        
+         </div>
+      </nav>
+      <div class="container-fluid jumbotron bg-white" >
+         <div class="row">
             <div class="col-md-4" >
-            
-            <div id="cC" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-            `+imgindk+`
-            </ol>
-            <div class="carousel-inner">
-            `+imgsrc+`
+               <div id="cC" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                  `+imgindk+`
+                  </ol>
+                  <div class="carousel-inner">
+                  `+imgsrc+`
+                  </div>
+                  <a class="carousel-control-prev" href="#cC" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Предыдущия</span>
+                  </a>
+                  <a class="carousel-control-next" href="#cC" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Следующия</span>
+                  </a>
+               </div>
             </div>
-            <a class="carousel-control-prev" href="#cC" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Предыдущия</span>
-            </a>
-            <a class="carousel-control-next" href="#cC" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Следующия</span>
-            </a>
-            </div>
-            
-            </div>
-        <div class="col-md-8 mt-4 mt-md-0" >
-            <table class="table table-bordered table-secondary" >
- 
-            `+table+`
- 
+            <div class="col-md-8 mt-4 mt-md-0" >
+               <table class="table table-bordered" style="background-color:#e9ecef;">
+              `+table+`
             </table>
-            
-                        <a href="javascript:void(0);" class="btn btn-sm btn-secondary"  id="more" >ещё</a>
-            
-        </div>
-        
-        
-        </div>
-<div class="row" >
-<div class="col-12 d-flex justify-content-end" >
-    <button id="rs" class="btn btn-primary btn-lg" >Заказать</button>
-</div>
-</div>
-     
-</div>
-
-
-
-    <div class="modal fade" id="contactDialog" tabindex="-1" role="dialog" aria-labelledby="contactDialogTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-    <div class="modal-header">
-    <h5 class="modal-title" id="contactDialogTitle">Свяжитесь с нами</h5>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-    </button>
-    </div>
-    <div class="modal-body ">
-    
-    <div class="text-center">ID вашего заказа <kbd>#<span id="ID">wait...</span></kbd></div>
-    <hr>
-    
-    <div class="d-flex justify-content-center" >
-    <div class="social instagram">
-    <a href="https://www.instagram.com/montenegro_arenda/" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
-    </div>
-    <div class="social facebook">
-    <a href="https://www.facebook.com/montenegroarenda/" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>    
-    </div>
-    <div class="social whatsapp">
-    <a href="https://wa.me/38268555972" target="_blank"><i class="fa fa-whatsapp fa-2x"></i></a>
-    </div>
-    <div class="social viber">
-    <a href="viber://chat?number=38268555972" target="_blank"><i class="fab fa-viber fa-2x"></i></a>    
-    </div>
-    </div>
-    </div>
-    <!-- пояснення
-    <div class="modal-footer d-block p-0 m-0">
-<span><small>*ID действует в течение 5 часов</small></span>
-
-    </div>
-    -->
-    </div>
-    </div>
-    </div>
-
-
-        <script src="js/data.js?hfkiz64jk"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-        <script src="js/all.js?hdbhdb"></script>
-        <script src="js/carsP.js?hjfdgj"></script> 
-
-</body>
-
+            <a href="javascript:void(0);" class="btn btn-sm btn-secondary"  id="more" >more</a>
+         </div>
+         
+         
+      </div>
+      
+      <div class="row" >
+         <div class="col-12 d-flex justify-content-end" >
+            <button id="rs" class="btn btn-primary btn-lg" >Заказать</button>
+         </div>
+      </div>
+      
+      </div>
+      
+   
+   <div class="modal fade" id="contactDialog" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="contactDialogTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5 class="modal-title" id="contactDialogTitle">Свяжитесь с нами</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body ">
+               <div class="text-center">ID вашего заказа <kbd>#<span id="ID">wait...</span></kbd></div>
+               <hr>
+               <div class="d-flex justify-content-center" >
+                  <div class="social instagram">
+                     <a href="https://www.instagram.com/montenegro_arenda/" target="_blank"><i class="fa fa-instagram fa-2x"></i></a>
+                  </div>
+                  <div class="social facebook">
+                     <a href="https://www.facebook.com/montenegroarenda/" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>    
+                  </div>
+                  <div class="social whatsapp">
+                     <a href="https://wa.me/38268555972" target="_blank"><i class="fa fa-whatsapp fa-2x"></i></a>
+                  </div>
+                  <div class="social viber">
+                     <a href="viber://chat?number=38268555972" target="_blank"><i class="fab fa-viber fa-2x"></i></a>    
+                  </div>
+               </div>
+            </div>
+            <!-- пояснення
+               <div class="modal-footer d-block p-0 m-0">
+               <span><small>*ID действует в течение 5 часов</small></span>
+               
+               </div>
+               -->
+         </div>
+      </div>
+   </div>
+   <script src="js/data.js?hfkiz64jk"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+   <script src="js/all.js?hdbhdb"></script>
+   <script src="js/carsP.js"></script>
+   </body>
 </html>
-
 `);
 
 
