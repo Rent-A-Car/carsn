35d4a1041e25d6a316a6363603c44b21f42c40bb
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-03-17 09:51:44 +0100

    Tue Mar 17 09:51:44 CET 2020

--- js/cars.js
+++ js/cars.js
@@ -259,7 +259,7 @@ myWindow.document.write(`
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-   <script src="js/all.js?hdbhdb"></script>
+   <script src="js/all.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js"></script>
    <script src="js/carsP.js"></script>
    </body>
