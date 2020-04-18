4760c01530584cd2e36125c2a7ed7e44517eca30
GORNOSTAY25 <volodia.palamar250405@gmail.com>
2020-04-18 16:18:03 +0200

    Sat Apr 18 16:18:03 CEST 2020

--- ua/js/carsP.js
+++ ua/js/carsP.js
@@ -213,7 +213,7 @@ document.write(`
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Оренда автомобілів (`+obj[i]["details"]["name"]+`)</title>
       <!--    CSS      -->
-      <link rel="stylesheet" href="css/all.css?vbnvcgnlj">
+      <link rel="stylesheet" href="/css/all.css?vbnvcgnlj">
       <style>
          .social a {
          text-align: center;
@@ -370,7 +370,7 @@ document.write(`
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-   <script src="js/all.js"></script>
+   <script src="/js/all.js"></script>
    <script src="js/carsP.js"></script>
    
 
